ff4ab328048d1a7b2bdab6523ce52460a876206b
abhishek17551 <ababhishek113@gmail.com>
2025-03-24 22:41:46 +0530

    extra options for queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Layout from './components/layout'
 import { ThemeProvider } from './context/theme-provider'
 import WeatherDashboard from './pages/WeatherDashboard'
 import CityPage from './pages/CityPage'
@@ -10,8 +9,18 @@ import {
 
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import Layout from './components/layout'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 function App() {
   return (
     <>
